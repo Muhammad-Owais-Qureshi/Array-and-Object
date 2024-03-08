55d1302c99a6628efd74c10c45155a94acfe4357
Muhammad Owais Qureshi <owaisqureshi34667@gmail.com>
2024-03-09 01:28:26 +0500

    Array and Object

---
+++ main.js
@@ -0,0 +1,16 @@
+// array type any
+var Arr = ['owais', 'imran', true, false, 12, 5, 8];
+console.log(Arr);
+// array type string
+var Arr2 = ['ali khan', 'rizwan', 'farooq'];
+console.log(Arr2);
+// array number
+var arr3 = [12.32, 1, 5];
+console.log(arr3);
+// typescript object 
+var Data = {
+    name: 'owais',
+    Class: 5,
+    vanue: "Governor House"
+};
+console.log(Data.name); // output owais
---
+++ main.ts
@@ -0,0 +1,17 @@
+// array type any
+let Arr : any [] = ['owais','imran',true,false,12,5,8]
+console.log(Arr)
+// array type string
+let Arr2 : string[] = ['ali khan','rizwan','farooq']
+console.log(Arr2)
+// array number
+let arr3 : number [] = [12.32,1,5]
+console.log(arr3)
+
+// typescript object 
+let Data = {
+    name: 'owais',
+    Class: 5,
+    vanue: "Governor House"
+}
+console.log(Data.name)  // output owais
